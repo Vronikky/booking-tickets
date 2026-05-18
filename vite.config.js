58f6dc9a879aa5215cf2f7b372dc4153c2144b1a
Nika <niklogacheva@yandex.ru>
2026-05-18 12:00:33 +0300

    vit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+    base: '/booking-tickets/',
     root: 'src',
     publicDir: '../public',
     server: {
