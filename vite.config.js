cf590adf46f324610b8f34b37e04249a8098e0c0
Nika <niklogacheva@yandex.ru>
2026-05-18 11:30:11 +0300

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
     root: 'src',
+    publicDir: '../public',
     server: {
         port: 5501,
         open: true
     },
     build: {
         outDir: '../dist',
-        emptyOutDir: true
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'src/index.html'),
+                korzina: resolve(__dirname, 'src/korzina.html'),
+                profile: resolve(__dirname, 'src/profile.html')
+            }
+        }
     }
 });
